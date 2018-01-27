d9d366a7241dc7ecd3d4f77be797fd9cace02b44
tmendonca28 <tmendonca28@gmail.com>
2018-01-28 00:39:53 +0300

    Adding the encryption function

--- caesar.py
+++ caesar.py
@@ -10,3 +10,47 @@ def getMode():
         else:
             print('Enter either "encrypt" or "e" or "decrypt" or "d".')
 
+def getMsg():
+    print('Enter your message:')
+    return input()
+
+def getKey():
+    key = 0
+    while True:
+        print('Enter the key number (ranging from 1-%s)' % (MAX_KEY_SIZE))
+        key = int(input())
+        if (key >= 1 and key <= MAX_KEY_SIZE):
+            return key
+
+def getTranslatedMessage(mode, message, key):
+    if mode[0] == 'd':
+        key = -key
+    translated = ''
+
+    for symbol in message:
+        if symbol.isalpha():
+            num = ord(symbol)
+            num += key
+
+            if symbol.isupper():
+                if num > ord('Z'):
+                    num -= 26
+                elif num < ord('A'):
+                    num += 26
+            elif symbol.islower():
+                if num > ord('z'):
+                    num -=26
+                elif num < ord('a'):
+                    num += 26
+
+            translated += chr(num)
+        else:
+            translated += symbol
+    return translated
+
+mode = getMode()
+message = getMsg()
+key = getKey()
+
+print('Your translated text is: ')
+print(getTranslatedMessage(mode, message, key))
